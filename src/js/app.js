1cedd87aa0a6c4effc680300caf6f6e4d36d8729
DmitrySm <smirnov.dmitry.a@mail.ru>
2018-06-28 14:20:04 +0300

    t1

--- src/js/app.js
+++ src/js/app.js
@@ -36,9 +36,6 @@ $(document).ready(()=>{
   let headWidth = 12
   let range = 0
 
-<<<<<<< HEAD
-  scene.enablePhysics(new V3(0,-9.81, 0), new BABYLON.OimoJSPlugin());
-=======
   let cameraPos = new V3(0, 2, -30)
   let mainCamera = createCamera(scene, cameraPos)
 
@@ -88,54 +85,7 @@ $(document).ready(()=>{
 
     heads.push(task.loadedMeshes[0])
   }
->>>>>>> parent of 59c878a... sh texture
 
-  let cameraPos = new V3(0, 2, -30)
-  let camera = createCamera(scene, cameraPos)
-  camera.alpha -= 0.6
-  camera.beta -= 0.6
-
-  var materialPlane = new BABYLON.StandardMaterial("solid", scene);
-  // materialPlane.diffuseTexture = new BABYLON.Texture('./assets/textures/pan.png', scene);
-	// materialPlane.emissiveTexture = new BABYLON.Texture(sURL, scene);
-	// materialPlane.emissiveTexture = new BABYLON.Texture('./assets/textures/pan.png', scene);
-	materialPlane.useAlphaFromDiffuseTexture
-	materialPlane.specularColor = new BABYLON.Color3(1, 0, 0);
-	materialPlane.backFaceCulling = true;//Allways show the front and the back of an element
-	// materialPlane.emissiveTexture.level = 0.1;  // powerfulness 
-
-
-  let lightPos = new V3(-5, 8, -10)
-  let light = addLight(scene, lightPos)
-  let shadows = addShadows(light)
-
-  let ground = createBox(scene, new V3(0, -0.4, 0), {width: 20, height: 0.4, depth: 20})
-  ground.material = materialPlane
-  ground.receiveShadows = true
-	ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.2 }, scene);
-
-
-  let bx = createBox(scene, new V3(0, 2, 0), {width: 2, height: 1, depth: 2})
-  shadows.addShadowCaster(bx);
-  // bx.receiveShadows = true
-	bx.physicsImpostor = new BABYLON.PhysicsImpostor(bx, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 2, restitution: 0.2 }, scene);
-  
-
-<<<<<<< HEAD
-  let body = createBox(scene, new V3(0, 5, 0), {width: 3, height: 1, depth: 1})
-  shadows.addShadowCaster(body);
-	body.physicsImpostor = new BABYLON.PhysicsImpostor(body, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 2, restitution: 0.2 }, scene);
-	
-
-  
-	var distanceJoint = new BABYLON.DistanceJoint({ maxDistance: 6, distance: 2});
-  bx.physicsImpostor.addJoint(body.physicsImpostor, distanceJoint);
-  body.physicsImpostor.addJoint(bx.physicsImpostor, distanceJoint);
-  
-  
-  // let lightScale = new V3(50, 80, 50)
-
-=======
   refTexture = new BABYLON.Texture("./assets/textures/pan.png", scene);
   refTexture.wrapU = BABYLON.Texture.CLAMP_ADDRESSMODE;
   refTexture.wrapV = BABYLON.Texture.CLAMP_ADDRESSMODE;
@@ -144,7 +94,6 @@ $(document).ready(()=>{
 
   let spsMaterial = createShaderMaterial(scene, mainCamera, 'circleRotate', mainTexture, refTexture, light)
   let sps = solidParticleSistem(scene, spsMaterial, createRibbon(scene), mainCamera, engine)
->>>>>>> parent of 59c878a... sh texture
 
 
 
@@ -156,13 +105,8 @@ $(document).ready(()=>{
   ////////
   engine.runRenderLoop(function () {
     fpsLable.innerHTML = Math.ceil(engine.getFps())
-<<<<<<< HEAD
-    // bx.position = new V3(scene.pointerX, scene.pointerY, 0)
-    time += 0.01
-=======
     bx.position = new V3(scene.pointerX, scene.pointerY, 0)
 
->>>>>>> parent of 59c878a... sh texture
     if (scene) {
       material.setFloat("time", time);
       materialPhong.setFloat("time", time);
@@ -179,8 +123,6 @@ $(document).ready(()=>{
       scene.render();
     }
   })
-<<<<<<< HEAD
-=======
 
   for (let i = 0; i < 3; i++) {
     ImportMesh(scene, '3d.babylon', onLoadSuccess, onLoadError)
@@ -196,5 +138,4 @@ $(document).ready(()=>{
       output.innerHTML = Math.ceil(this.value/100 * (heads.length - 1) * 10) / 10 + 1
       range = this.value
   }
->>>>>>> parent of 59c878a... sh texture
 })
